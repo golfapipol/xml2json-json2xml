ecad287217fa41185e60a2d931e4a94b030aa612
Apipol Sukgler <apipol@computerlogy.com>
2018-08-16 12:39:29 +0700

    [Created]: add test to json

--- xml2json_test.go
+++ xml2json_test.go
@@ -1,18 +1,19 @@
 package json2xml_test
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"io/ioutil"
 	"testing"
 )
 
 type GetCountriesAvailable struct {
-	CountryCode []CountryCode `xml:Body>GetCountriesAvailableResult>CountryCode`
+	CountryCode []CountryCode `xml:"Body>GetCountriesAvailableResponse>GetCountriesAvailableResult>CountryCode" json:"countries"`
 }
 
 type CountryCode struct {
-	Code        string `xml:"Code"`
-	Description string `xml:"Description"`
+	Code        string `xml:"Code" json:"code"`
+	Description string `xml:"Description" json:"description"`
 }
 
 func Test_ConvertXML_Input_XML_Should_Be_Struct(t *testing.T) {
@@ -28,20 +29,6 @@ func Test_ConvertXML_Input_XML_Should_Be_Struct(t *testing.T) {
 			CountryCode{"UnitedStates", "United States"},
 		},
 	}
-	// Envelope{
-	// 	Body: Body{
-	// 		GetCountriesAvailableResponse: GetCountriesAvailableResponse{
-	// 			CountryCode: []CountryCode{
-	// 				CountryCode{"Canada", "Canada"},
-	// 				CountryCode{"GreatBritain", "Great Britain and Wales"},
-	// 				CountryCode{"IrelandNorthern", "Northern Ireland"},
-	// 				CountryCode{"IrelandRepublicOf", "Republic of Ireland"},
-	// 				CountryCode{"Scotland", "Scotland"},
-	// 				CountryCode{"UnitedStates", "United States"},
-	// 			},
-	// 		},
-	// 	},
-	// }
 
 	xml.Unmarshal(xmlFile, &actual)
 
@@ -50,5 +37,23 @@ func Test_ConvertXML_Input_XML_Should_Be_Struct(t *testing.T) {
 		if actualCountryCode != expectedCountryCode {
 			t.Errorf("expected at index: %d %s but it got %s", index, expectedCountryCode, actualCountryCode)
 		}
+	}
+}
+
+func Test_ToJSON_Should_Be_JSON(t *testing.T) {
+	expected := `{"countries":[{"code":"Canada","description":"Canada"},{"code":"GreatBritain","description":"Great Britain and Wales"},{"code":"IrelandNorthern","description":"Northern Ireland"},{"code":"IrelandRepublicOf","description":"Republic of Ireland"},{"code":"Scotland","description":"Scotland"},{"code":"UnitedStates","description":"United States"}]}`
+	getCountriesAvailable := GetCountriesAvailable{
+		CountryCode: []CountryCode{
+			CountryCode{"Canada", "Canada"},
+			CountryCode{"GreatBritain", "Great Britain and Wales"},
+			CountryCode{"IrelandNorthern", "Northern Ireland"},
+			CountryCode{"IrelandRepublicOf", "Republic of Ireland"},
+			CountryCode{"Scotland", "Scotland"},
+			CountryCode{"UnitedStates", "United States"},
+		},
+	}
+	actual, _ := json.Marshal(getCountriesAvailable)
+	if expected != string(actual) {
+		t.Errorf("expected \n%s but it got \n%s", expected, actual)
 	}
 }
